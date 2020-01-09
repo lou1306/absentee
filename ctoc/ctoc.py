8ca807a6bda4593673e3b56f74efdc232cb2254c
Luca Di Stefano <luca.distefano@gssi.it>
2020-01-09 12:36:38 +0100

    Error classes

--- ctoc/ctoc.py
+++ ctoc/ctoc.py
@@ -10,9 +10,39 @@ import toml
 import click
 
 
-class ConfigError(Exception):
+class BaseError(Exception):
+    def handle(self):
+        click.echo(f"{self.HEADER} error: {self.message}", err=True)
+        exit(self.CODE)
+
+
+class ConfigError(BaseError):
+    HEADER = "Configuration"
+    CODE = 1
+
     def __init__(self, message):
         self.message = message
+
+    def __str__(self):
+        return f"Configuration error: {self.message}"
+
+
+class TransformError(BaseError):
+    HEADER = "Transformation"
+    CODE = 6
+
+    def __init__(self, message, coords):
+        self.message = f"{message}\nat: {coords}"
+        self.coords = coords
+
+
+class ParseError(BaseError):
+    HEADER = "Parsing"
+    CODE = 10
+
+    def __init__(self, ply_exception):
+        self.message = ply_exception
+
 
 
 class Transformation(NodeVisitor):
